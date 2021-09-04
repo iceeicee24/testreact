7d38582e7fd9b447b0b8b9d90b9ee5570a72acd9
isaac viray <isaac.j.viray@gmail.com>
2021-09-04 12:14:19 +1000

    updated code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { render } from 'react-dom';
+
+// global stylesheet
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { initFacebookSdk, jwtInterceptor, errorInterceptor, history } from './_helpers';
+import { App } from './App';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// setup fake backend
+import { fakeBackend } from './_helpers';
+fakeBackend();
+
+// enable interceptors for http requests
+jwtInterceptor();
+errorInterceptor();
+
+// wait for facebook sdk before startup
+initFacebookSdk().then(startApp);
+
+function startApp() { 
+    render(
+        <Router history={history}>
+            <App />
+        </Router>,
+        document.getElementById('app')
+    );
+}
